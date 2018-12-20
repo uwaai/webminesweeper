a8c9e1337b148400a1f2963278f660fc1eaf0d89
uwaai <46029408+uwaai@users.noreply.github.com>
2018-12-21 01:28:03 +0900

    UPDATE FILES
    UPDATE FILES

---
+++ js/model.js
@@ -0,0 +1,158 @@
+﻿var Model = function(w, h, bombnum){
+	//init
+	this.listeners = [];
+	this.w = w;
+	this.h = h;
+	this.bombnum = bombnum;
+	this.clicked = []; //0:close, 1:open, 2:flag
+	this.data = [];  //-1:bomb, 0-8:bombnum
+	this.endstatus = 0; //0:notEnd, 1:win, 2:lose
+	this.endrow = -1;
+	this.endcell = -1;
+	for(var i=0; i<h+2; i++){
+		this.clicked.push([]);
+		this.data.push([]);
+		for(var j=0; j<w+2; j++){
+			this.data[i].push(0);
+			this.clicked[i].push(0);
+		}
+	}
+	//bomb set
+	var r1 = [];
+	for(var i=0; i<w*h;i++){
+		r1.push(i);
+	}
+	for(var i=0; i<bombnum; i++){
+		var rind = Math.floor(Math.random()*r1.length);
+		var val = r1[rind];
+		var rc = val%w;
+		var rr = Math.floor(val/w);
+		this.data[rr+1][rc+1]=-1;
+		r1.splice(rind, 1);
+	}
+	//number set
+	for(var i=1; i<h+1; i++){
+		for(var j=1; j<w+1; j++){
+			if(this.data[i][j]!=0)continue;
+			var ctr = 0;
+			if(this.data[i-1][j-1]==-1) ctr++;
+			if(this.data[i-1][j  ]==-1) ctr++;
+			if(this.data[i-1][j+1]==-1) ctr++;
+			if(this.data[i  ][j-1]==-1) ctr++;
+			if(this.data[i  ][j+1]==-1) ctr++;
+			if(this.data[i+1][j-1]==-1) ctr++;
+			if(this.data[i+1][j  ]==-1) ctr++;
+			if(this.data[i+1][j+1]==-1) ctr++;
+			this.data[i][j] = ctr;
+		}
+	}
+	//outside set
+	for(var i=0; i<w+2; i++){
+		this.clicked[0][i] = 1;
+		this.clicked[h+1][i] = 1;
+	}
+	for(var i=0; i<h+2; i++){
+		this.clicked[i][0] = 1;
+		this.clicked[i][w+1] = 1;
+	}
+};
+
+Model.prototype.out = function(l){
+	for(var i=0; i<this.h+2; i++){
+		console.log(this.data[i]);
+	}
+};
+
+Model.prototype.addListener = function(l){
+	return this.listeners.push(l);
+};
+
+Model.prototype.update = function(){
+	var ls = this.listeners;
+	for(var i=0; i<ls.length; i++){
+		ls[i].update(this);
+	}
+};
+
+Model.prototype.isOpened = function(r, c){
+	return this.clicked[r+1][c+1] == 1;
+};
+
+Model.prototype.isFlaged = function(r, c){
+	return this.clicked[r+1][c+1] == 2;
+};
+
+Model.prototype.get = function(r, c){
+	return this.data[r+1][c+1];
+};
+
+Model.prototype.open = function(r, c){
+	var mr = r+1;
+	var mc = c+1;
+	if(this.isOpened(r, c))return;
+	this.clicked[mr][mc]=1;
+	//lose?
+	if(this.data[mr][mc]==-1) {
+		this.lose(r, c);
+		return;
+	}
+	if(this.data[mr][mc]==0){
+		this.open(r+1,c);
+		this.open(r-1,c);
+		this.open(r,c+1);
+		this.open(r,c-1);
+	}
+	//win?
+	if(this.w * this.h - this.getOpenNum() == this.bombnum){
+		this.win();
+	}
+};
+
+Model.prototype.setFlag = function(r, c){
+	this.clicked[r+1][c+1]=2;
+};
+
+Model.prototype.removeFlag = function(r, c){
+	this.clicked[r+1][c+1]=0;
+};
+
+Model.prototype.win = function(){
+	this.endstatus = 1;
+};
+
+Model.prototype.lose = function(r, c){
+	this.endstatus = 2;
+	this.endrow = r;
+    this.endcell = c;
+    var w = this.w;
+    var h = this.h;
+	for(var i=0; i< h+2; i++){
+		for(var j=0; j< w+2; j++){
+            this.clicked[i][j] = 1;
+		}
+	}
+};
+
+Model.prototype.getOpenNum = function(){
+    var ctr = 0;
+    var h = this.h;
+    var w = this.w;
+    for(var i=1; i< h+1; i++){
+		for(var j=1; j< w+1; j++){
+            if(this.clicked[i][j]==1)ctr++;
+		}
+    }
+    return ctr;
+};
+
+Model.prototype.getFlagNum = function(){
+    var ctr = 0;
+    var h = this.h;
+    var w = this.w;
+    for(var i=1; i< h+1; i++){
+		for(var j=1; j< w+1; j++){
+            if(this.clicked[i][j]==2)ctr++;
+		}
+    }
+    return ctr;
+};
---
+++ js/view.js
@@ -0,0 +1,108 @@
+﻿var load = function(){
+	var model = new Model(10, 10, 10);
+	var view = new View(
+		document.getElementById("ms"),
+		document.getElementById("openbtn"),
+        document.getElementById("flagbtn"),
+        document.getElementById("textarea"),
+		model);
+};
+
+var View=function(table, openbtn, flagbtn, textarea, model){
+	model.addListener(this);
+	this.haveFlag = false;
+	openbtn.style.backgroundColor = "black";
+	flagbtn.style.backgroundColor = "white";
+	var view = this;
+	openbtn.onclick = function(){
+		openbtn.style.backgroundColor = "black";
+		flagbtn.style.backgroundColor = "white";
+		view.haveFlag = false;
+	};
+	flagbtn.onclick = function(){
+		openbtn.style.backgroundColor = "white";
+		flagbtn.style.backgroundColor = "black";
+		view.haveFlag = true;
+    };
+    this.textarea = textarea;
+	this.table = table;
+	model.update();
+};
+
+View.prototype.update = function(model){
+	var h = model.h
+	var w = model.w;
+	var table = this.table;
+	//remove all children
+	while(table.firstChild){
+		table.removeChild(table.firstChild);
+	}
+	//set row, cell, button or div
+	for(var i=0; i<h; i++){
+		var row = table.insertRow(-1);
+		for(var j=0; j<w; j++){
+			var cell = row.insertCell(-1);
+			//div
+			if(model.isOpened(i, j)){
+				var div = document.createElement("div");
+				div.innerHTML = this._conv(model.get(i, j));
+				cell.appendChild(div);
+			//button
+			}else{
+				var btn = document.createElement("button");
+				model.isFlaged(i, j) ? btn.innerHTML = "■" : btn.innerHTML = "□";
+				if(model.endstatus==0)btn.onclick = this.onclick(model, i, j);
+				cell.appendChild(btn);
+			}
+		}
+    }
+    var f = model.bombnum - model.getFlagNum();
+    this.textarea.innerHTML = "残り：" + f;
+	//win
+	if(model.endstatus==1){
+		this.textarea.innerHTML = "YOU WIN!!";
+	}
+	//lose
+	if(model.endstatus==2){
+		this.table.rows[model.endrow].cells[model.endcell].firstChild.style.backgroundColor = "red";
+		this.textarea.innerHTML = "YOU LOSE!!";
+	}
+};
+
+View.prototype.onclick = function(model, r, c){
+	var view = this;
+	return function(){
+		if(model.isFlaged(r,c)){
+			if(view.haveFlag){
+				model.removeFlag(r, c);
+			}
+			else{
+				//Do Nothing
+			}
+		}else{
+			if(view.haveFlag){
+				model.setFlag(r, c);
+			}
+			else{
+				model.open(r, c);
+			}
+		}
+		model.update();
+	};
+};
+
+View.prototype._conv = function(number){
+	switch(number){
+		case -1: return "●";
+		case  0: return "□";
+		case  1: return "１";
+		case  2: return "２";
+		case  3: return "３";
+		case  4: return "４";
+		case  5: return "５";
+		case  6: return "６";
+		case  7: return "７";
+		case  8: return "８";
+	}
+	return "Ｎ";
+};
